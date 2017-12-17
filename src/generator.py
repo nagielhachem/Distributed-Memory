2f2a0ec9e41b3ad4935c3143611437a8943f1c85
Daval-G <guillaume.daval-frerot@epita.fr>
2017-12-17 15:29:29 +0100

    Fix README, add random number generator

---
+++ src/generator.py
@@ -0,0 +1,24 @@
+import sys
+
+from numpy import random
+
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 3):
+        print("Format: %s size seed" % sys.argv[0])
+        exit(1)
+
+    size = int(sys.argv[1])
+    seed = int(sys.argv[2])
+
+    random.seed(seed)
+    f    = open("./random.txt", "w")
+    buf  = ""
+    for i in range(size // 1000 + 1):
+        rand = random.randint(size, size=min(1000, size - i * 1000))
+        buf  = ""
+        for j in rand:
+            buf += "%d\n" % j
+        f.write(buf)
+    f.close()
